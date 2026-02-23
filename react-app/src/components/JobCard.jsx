d4b16bb5ccc0a21d67095e521037a9bb5b0d51e8
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2026-02-23 12:51:08 -0800

    feat: Add JobCard component with title, company, type, and salary details and basic styling

---
+++ react-app/src/components/JobCard.jsx
@@ -0,0 +1,14 @@
+import "./JobCard.css"
+
+function JobCard({ title, company, jobType, salary }) {
+    return(
+        <div className="job-card"> 
+            <h3>{title}</h3>
+            <p>{company} - {jobType}</p>
+            <p>{salary}</p>
+            <button>View More Details</button>
+        </div>
+    );
+}
+
+export default JobCard;
