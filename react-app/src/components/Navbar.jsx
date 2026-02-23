9905f5605ae488fb82f8e87a028ec25a1409162c
Eric Harrison <hericeh72@gmail.com>
2026-02-23 14:23:51 -0800

    feat: Updated navbar component to handle separate navbars based on user's role

--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -1,45 +1,50 @@
 import "../styles/Navbar.css"
 
-function Navbar() {
+function Navbar({ role }) {
+    const browseButtonText = (role=="employer") ? "View Listing" : "Browse Jobs" 
 
-    const jobBoard = () => {
-        // Redirect to job board page
-    }
-
-
-    const register = () => {
-        // Redirect to register page
-    }
-
-    const login = () => {
-        // Redirect to login page
-    }
-
-    const browseJobs = () => {
-        // Redirect to browse jobs page
-    }
+    const handleRedirect = (page) => {
+        // Redirect to page
+        alert(page);
+    };
 
   return (
-    <>
-        <div className="Navbar-div">
+    <div className="navbar">
+        <button onClick={() => handleRedirect("job board")} className="navbar-button">
+            Job Board
+        </button>
 
-            <button onClick={jobBoard} className="Navbar-button">
-                Job Board
-            </button>
-
-            <span>
-                <button onClick={register} className="Navbar-button" id="gray-button">
+        <span>
+            <div className="buttons-right">
+                {!role && (<div><button onClick={() => handleRedirect("register")} className="navbar-button" id="gray-button">
                     Register
                 </button>
-                <button onClick={login} className="Navbar-button" id="gray-button">
+                <button onClick={() => handleRedirect("login")} className="navbar-button" id="gray-button">
                     Login
+                </button></div>)}
+
+                {role && (<button onClick={() => handleRedirect("logout")} className="navbar-button" id="gray-button">
+                    Logout
+                </button>)}
+
+                {role=="jobSeeker" && (<button onClick={() => handleRedirect("profile")} className="navbar-button" id="gray-button">
+                    Profile
+                </button>)}
+
+
+                {role=="admin" && (<div><button onClick={() => handleRedirect("analytics")} className="navbar-button" id="gray-button">
+                    Analytics
                 </button>
-                <button onClick={browseJobs} className="Navbar-button">
-                    Browse Jobs
+                <button onClick={() => handleRedirect("users")} className="navbar-button" id="gray-button">
+                    Users
+                </button></div>)}
+
+                <button onClick={() => handleRedirect("browse jobs")} className="navbar-button">
+                    {browseButtonText}
                 </button>
-            </span>
-        </div>
-    </>
+            </div>
+        </span>
+    </div>
   )
 }
 
